e8cbaa7c84058ca8b509e65378b61ea95ef1254f
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-20 18:47:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist_name
+  
   @@all = []
 
   def self.all
@@ -9,5 +10,34 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new 
+    @@all << song 
+  end
+  
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    song = self.new 
+    song.name = song_name
+    song 
+    @@all << song 
+  end
+  
+  def self.find_by_name(find_name)
+    self.all.each do |song|
+      if song.name == find_name
+        return song 
+      end
+    end
+  end
+  
+  
+  
+  
 end
